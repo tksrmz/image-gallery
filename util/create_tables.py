341002bcdf0e273433974399530f49125216a18f
taku-shiromizu <shiromizu.taku.paramedic718@passinbox.com>
2024-02-14 23:41:15 +0900

    table migration

--- util/create_tables.py
+++ util/create_tables.py
@@ -11,37 +11,34 @@ c = conn.cursor()
 # Create images table
 c.execute('''
             CREATE TABLE IF NOT EXISTS images (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
+                name TEXT PRIMARY KEY NOT NULL,
                 extension TEXT NOT NULL,
-                title_id INTEGER,
-                FOREIGN KEY(title_id) REFERENCES titles(id)
+                title_name TEXT,
+                FOREIGN KEY(title_name) REFERENCES titles(name)
             )
           ''')
 
 # Create titles table
 c.execute('''
             CREATE TABLE IF NOT EXISTS titles (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL
+                name TEXT PRIMARY KEY NOT NULL
             )
           ''')
 
 # Create tags table
 c.execute('''
             CREATE TABLE IF NOT EXISTS tags (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL
+                name TEXT PRIMARY KEY NOT NULL
             )
           ''')
 
 # Create image_tags table
 c.execute('''
             CREATE TABLE IF NOT EXISTS image_tags (
-                image_id INTEGER,
-                tag_id INTEGER,
-                FOREIGN KEY(image_id) REFERENCES images(id),
-                FOREIGN KEY(tag_id) REFERENCES tags(id)
+                image_name TEXT NOT NULL,
+                tag_name TEXT NOT NULL,
+                FOREIGN KEY(image_name) REFERENCES images(name),
+                FOREIGN KEY(tag_name) REFERENCES tags(name)
             )
           ''')
 
@@ -49,8 +46,8 @@ c.execute('''
 c.execute('''
             CREATE TABLE IF NOT EXISTS hashes (
                 hash TEXT PRIMARY KEY,
-                image_id INTEGER,
-                FOREIGN KEY(image_id) REFERENCES images(id)
+                image_name TEXT NOT NULL,
+                FOREIGN KEY(image_name) REFERENCES images(name)
             )
           ''')
 
